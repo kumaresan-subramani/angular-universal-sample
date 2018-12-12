2ba57fd5a3aa5ad351a6647fcad71a3feab35e05
ROOT\kumaresan.subramani <kumaresan.subramani@syncfusion.com>
2018-12-12 10:41:40 +0530

    feature: updated source

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core';
+import { data } from './data';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Universal Demo using Angular and Angular CLI</h1>
+  <a routerLink="/">Home</a>
+  <a routerLink="/lazy">Lazy</a>
+  <a routerLink="/lazy/nested">Lazy_Nested</a>
+  <router-outlet></router-outlet>
+  <ejs-grid [dataSource]='data'>
+                <e-columns>
+                    <e-column field='OrderID' headerText='Order ID' textAlign='Right' width=90></e-column>
+                    <e-column field='CustomerID' headerText='Customer ID' width=120></e-column>
+                    <e-column field='Freight' headerText='Freight' textAlign='Right' format='C2' width=90></e-column>
+                    <e-column field='OrderDate' headerText='Order Date' textAlign='Right' format='yMd' width=120></e-column>
+                </e-columns>
+                </ejs-grid>
+  `,
+  styles: []
+})
+export class AppComponent {
+  public data: Object[];
+
+  ngOnInit(): void {
+    this.data = data;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TransferHttpCacheModule } from '@nguniversal/common';
+import { GridModule } from '@syncfusion/ej2-angular-grids';
+import { PageService, SortService, FilterService, GroupService } from '@syncfusion/ej2-angular-grids';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [GridModule,
+    BrowserModule.withServerTransition({ appId: 'my-app' }),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule' },
+      { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule' }
+    ]),
+    TransferHttpCacheModule,
+  ],
+  providers: [PageService,
+    SortService,
+    FilterService,
+    GroupService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
